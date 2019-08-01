7ff47c0d7d2e4acbfa1c110d5316537be5d9b23f
Hich <hichlist@yandex.ru>
2019-08-01 17:16:03 +0300

    Required auth is added

--- main/views.py
+++ main/views.py
@@ -2,17 +2,21 @@ from django.shortcuts import render
 from rest_framework import generics
 from .serializers import LampPostSerializer
 from .models import LampPost
+from rest_framework.permissions import IsAuthenticated
 
 
 class LampPostCreateView(generics.CreateAPIView):
     serializer_class = LampPostSerializer
+    permission_classes = (IsAuthenticated, )
 
 
 class LampPostListView(generics.ListAPIView):
     serializer_class = LampPostSerializer
     queryset = LampPost.objects.all()
+    permission_classes = (IsAuthenticated, )
 
 
 class LampPostUpdateView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = LampPostSerializer
     queryset = LampPost.objects.all()
+    permission_classes = (IsAuthenticated, )

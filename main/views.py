d3948674b1810d44ec12630f05bf6da3c054a377
Hich <hichlist@yandex.ru>
2019-08-02 16:45:36 +0300

    Smart modify request data is added

--- main/views.py
+++ main/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from rest_framework import generics
 from .serializers import LampPostSerializer
+from .models import LampPost
+from rest_framework.permissions import IsAuthenticated
+from datetime import datetime
 
 
 class LampPostCreateView(generics.CreateAPIView):
     serializer_class = LampPostSerializer
+    permission_classes = (IsAuthenticated, )
 
+    def post(self, request, *args, **kwargs):
+
+        print('is_on' in request.POST)
+        if 'is_on' in request.POST:
+            if request.data['is_on'] == 'true':
+                request.POST._mutable = True
+                request.POST['on_time'] = datetime.now()
+        else:
+            request.POST._mutable = True
+            request.POST['off_time'] = datetime.now()
+            request.POST['brightness'] = 0
+
+        return self.create(request, *args, **kwargs)
+
+
+class LampPostListView(generics.ListAPIView):
+    serializer_class = LampPostSerializer
+    queryset = LampPost.objects.all()
+    permission_classes = (IsAuthenticated, )
+
+
+class LampPostUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = LampPostSerializer
+    queryset = LampPost.objects.all()
+    permission_classes = (IsAuthenticated, )
